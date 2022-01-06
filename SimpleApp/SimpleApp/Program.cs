7a8d13a69b0040f897bf4ea4c968a53b6c78d7ac
Sam Wu <swu@nait.ca>
2022-01-06 16:20:29 -0700

    Added Write method

--- SimpleApp/SimpleApp/Program.cs
+++ SimpleApp/SimpleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello can you C#?");
 Console.WriteLine("without your eye glasses?");
+Console.Write("Can see the difference");
+Console.Write(" between Write and WriteLine");
