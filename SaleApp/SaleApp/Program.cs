cc2cc796170aea3ea1d8871a177aee7075539cc1
Sam Wu <swu@nait.ca>
2022-01-12 16:50:35 -0700

    End of jan 12

--- SaleApp/SaleApp/Program.cs
+++ SaleApp/SaleApp/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*  Purpose:    Compute the total of a sales item using the price and tax.
+ *  
+ *  Inputs:     price in dollars and cents
+ *  
+ *  Outputs:    sale tax
+ *              sales total     
+ * 
+ * Algorithm:   Step 1) Prompt and read in the sale price
+ *              Step 2) Calculate the sale total using the formula:
+ *                          salesTax = salePrice * 0.05
+ *                          saleTotal = salePrice + salesTax
+ *              Step 3) Display the computed sale total        
+ * 
+ * Last modified:   2022-01-12
+ * */
+using System;
 
 namespace SaleApp
 {
@@ -6,19 +21,36 @@ namespace SaleApp
     {
         static void Main(string[] args)
         {
-            // Declare variables for the price, tax, and total.
-            double price,
-                    tax,
-                    total;
-            // Assign values to the varaibles for price, tax, and total
-            price = 29.75;
-            tax = 1.76;
-            total = 31.51;
+            // Declare variables for inputs and outputs
+            double salePrice;
+            const double SaleTaxRate = 0.05;
+            double saleTax;
+            double saleTotal;
+            string userInput;
 
-            // Write to the screen the price, tax, and total.
-            Console.WriteLine($"The price of the item is {price:C}");
-            Console.WriteLine($"The tax is {tax:C}");
-            Console.WriteLine($"The total is {total:C}");
+            // Step 1) Prompt and read in the sale price
+            Console.Write("Enter the sale price: ");
+            userInput = Console.ReadLine();
+            // Convert userInput from a string type to a double type
+            salePrice = double.Parse(userInput);
+            //salePrice = Convert.ToDouble(userInput);
+            //salePrice = double.Parse(Console.ReadLine());
+
+
+            // Step 2) Calculate the sale total and saleTax
+            saleTax = salePrice * SaleTaxRate;
+            saleTotal = salePrice + saleTax;
+
+            //  Step 3) Display the computed sale tax and total
+            /*   1234567890  12345678       
+             *   Price:        $29.75
+             *   Tax:           $1.76
+             *   Total:        $31.51
+             * */
+            Console.WriteLine($"{"Price:", -10} {salePrice, 8:C}");
+            Console.WriteLine($"{"Tax:", -10} {saleTax,8:C}");
+            Console.WriteLine($"{"Total", -10} {saleTotal,8:C}");
+            
 
         }
     }
---
+++ WagesApp/WagesApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*  Purpose:    Compute the weekly wage of an employee.
+ *  
+ *  Inputs:     regular hourly rate
+ *              regular hours worked
+ *              overtime hourly rate
+ *              overtime hours worked
+ *              
+ *  Outputs:    regularWages
+ *              overtimeWages
+ *              
+ *  Algorithm:  Step 1: Prompt for regular hourly rate,
+ *                  regular hours worked,
+ *                  overtime hourly rate,
+ *                  overtime hours worked
+ *              Step 2: Compute the following values:
+ *                 regularWages = regularHourlyRate * regularHoursWorked
+ *                 overtimeWages = overtimeHourlyRate * overtimeHoursWorked
+ *                 totalWages = regularWages + overtimeWages
+ *              Step 3: Display the wages for the week   
+ * */
+// Declare variables for inputs and outputs
+double regularHoursWorked;
+double regularHourlyRate;
+double overtimeHoursWorked;
+double overtimeHourlyRate;
+double regularWages,
+        overtimeWages,
+        totalWages;
+
+// Prompt and read in the values for regular hour rate
+//Console.Write
